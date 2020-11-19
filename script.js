94019d0d8487cdcdc2ab38db81d94f6ff380c407
Esu Gabriel <orionesu.dev@gmail.com>
2020-11-19 13:44:37 +0100

    Added functionalities to nav bar

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
 const navSlide = () =>{
     const burger = document.querySelector('.hamburger-menu');
     const nav = document.querySelector('.mobile-nav');
+    const navLinks = document.querySelectorAll('.mobile-nav-list li');
+    const navIcon = document.querySelectorAll('.mobile-social-icons span');
 
     burger.addEventListener('click', () =>{
         nav.classList.toggle('nav-active');
+
+        navLinks.forEach((link, index) => {
+            if(link.style.animation){
+                link.style.animation = '';
+            }else{
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7  + .3}s`;
+            }
+        });
+
+        navIcon.forEach((link, index) => {
+            if(link.style.animation){
+                link.style.animation = '';
+            }else{
+                link.style.animation = `navIconFade 1.7s ease forwards ${index / 7  + 1}s`;
+            }
+        });
+
+        burger.classList.toggle('toggle');
     });
+
 }
 
 navSlide();
