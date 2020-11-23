eb73ecf1a383e12f349db0bd653c2458e5f7b279
Esu Gabriel <orionesu.dev@gmail.com>
2020-11-23 07:34:45 +0100

    ::contruction

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const navSlide = () =>{
             if(link.style.animation){
                 link.style.animation = '';
             }else{
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7  + .3}s`;
+                link.style.animation = `navLinkFade 0.8s ease forwards ${index / 7  + .5}s`;
             }
         });
 
